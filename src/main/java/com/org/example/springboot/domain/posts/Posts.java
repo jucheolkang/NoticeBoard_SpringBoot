427f087f21542fd08e0e32634d6253bc39aa3b81
jucheolkang <87826365+jucheolkang@users.noreply.github.com>
2022-07-16 09:24:35 +0900

    Add files via upload

---
+++ src/main/java/com/org/example/springboot/domain/posts/Posts.java
@@ -0,0 +1,43 @@
+package com.org.example.springboot.domain.posts;
+
+import com.org.example.springboot.domain.posts.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+
+    }
+
+    public void updata(String title,String content){
+        this.title = title;
+        this.content = content;
+    }
+
+}
+
+
---
+++ src/main/java/com/org/example/springboot/service/posts/PostsService.java
@@ -0,0 +1,47 @@
+package com.org.example.springboot.service.posts;
+
+import com.org.example.springboot.domain.posts.Posts;
+import com.org.example.springboot.domain.posts.PostsRepository;
+import com.org.example.springboot.web.dto.PostsListResponseDto;
+import com.org.example.springboot.web.dto.PostsResponseDto;
+import com.org.example.springboot.web.dto.PostsSaveRequestDto;
+
+import com.org.example.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.stream.Collectors;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. .id = "+id));
+        posts.updata(requestDto.getTitle(),requestDto.getContent());
+        return id;
+    }
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id =" + id));
+        return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+
+    }
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+
+    }
+}
